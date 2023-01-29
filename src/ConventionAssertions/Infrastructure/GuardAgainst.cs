a754829b6ba8dd0075c88449421aecc3b026d355
niklaswendel <niklas.wendel@gmail.com>
2023-01-29 16:37:16 +0100

    Remove unused code

--- src/ConventionAssertions/Infrastructure/GuardAgainst.cs
+++ src/ConventionAssertions/Infrastructure/GuardAgainst.cs
@@ -4,14 +4,6 @@ namespace ConventionAssertions.Infrastructure;
 
 internal static class GuardAgainst
 {
-    public static void Condition([DoesNotReturnIf(true)] bool condition, string message, string argumentName)
-    {
-        if (condition)
-        {
-            throw new ArgumentException(message, argumentName);
-        }
-    }
-
     public static void Null<T>([NotNull] T? value, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : class
     {
@@ -26,16 +18,6 @@ internal static class GuardAgainst
         if (string.IsNullOrWhiteSpace(value))
         {
             throw new ArgumentNullException(argumentName);
-        }
-    }
-
-    public static void Undefined<T>(T value, [CallerArgumentExpression("value")] string? argumentName = null)
-        where T : struct, Enum
-    {
-        var isDefined = Enum.IsDefined(value);
-        if (!isDefined)
-        {
-            throw new ArgumentOutOfRangeException(argumentName, $"Value {value} is not valid for type {typeof(T).Name}");
         }
     }
 }
