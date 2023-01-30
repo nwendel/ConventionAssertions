7611742dadf16e40d5d9968e74a4ebfcac8ed364
niklaswendel <niklas.wendel@gmail.com>
2023-01-30 19:28:42 +0100

    Enforce code styles for tests

---
+++ src/ConventionAssertions.Tests/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Unit test naming standard")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Required by some unit tests")]
+[assembly: SuppressMessage("Performance", "CA1852:Seal internal types", Justification = "Required by some unit tests")]
