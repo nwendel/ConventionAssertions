b13b2467b6f4e6b75abfb32a03e75f95000e9b35
Niklas Wendel <niklas.wendel@gmail.com>
2023-02-01 10:43:27 +0100

    Added IsNotNestedInternal type convetion (#10)

---
+++ src/ConventionAssertions.Tests/Rules/TestHelpers/InternalDeclaringClass.cs
@@ -0,0 +1,28 @@
+﻿namespace ConventionAssertions.Tests.Rules.TestHelpers;
+
+internal class InternalDeclaringClass
+{
+    public class NestedPublicClass
+    {
+    }
+
+    internal class NestedInternalClass
+    {
+    }
+
+    protected internal class NestedProtectedInternalClass
+    {
+    }
+
+    protected class NestedProtectedClass
+    {
+    }
+
+    private protected class NestedPrivateProtectedClass
+    {
+    }
+
+    private class NestedPrivateClass
+    {
+    }
+}
