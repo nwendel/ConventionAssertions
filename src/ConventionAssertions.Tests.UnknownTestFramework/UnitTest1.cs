9f94ede4c1d62b5e1ca81a3618a824923f55a799
niklaswendel <niklas.wendel@gmail.com>
2023-02-01 07:45:50 +0100

    Added framework tests

--- src/ConventionAssertions.Tests.UnknownTestFramework/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConventionAssertions.Tests.UnknownTestFramework;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
--- src/ConventionAssertions.Tests.XunitTestFramework/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConventionAssertions.Tests.XunitTestFramework;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
