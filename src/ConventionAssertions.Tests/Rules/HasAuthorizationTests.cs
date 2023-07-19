18eb9cc175a7c1d512e0e847f02652e200f522af
niklaswendel <niklas.wendel@gmail.com>
2023-07-19 09:12:34 +0200

    Added aspnetcore authorization rule

---
+++ src/ConventionAssertions.Tests/Rules/HasAuthorizationTests.cs
@@ -0,0 +1,130 @@
+﻿using ConventionAssertions.Internal;
+using ConventionAssertions.Rules;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConventionAssertions.Tests.Rules;
+
+public class HasAuthorizationTests
+{
+    private readonly HasAuthorization _tested = new();
+    private readonly ConventionContext _context = new();
+
+    [Fact]
+    public void Can_assert_class_authorize()
+    {
+        var method =
+            typeof(AuthorizeController)
+            .GetMethod(nameof(AuthorizeController.Method))!;
+
+        _tested.Assert(method, _context);
+
+        Assert.Empty(_context.Messages);
+    }
+
+    [Fact]
+    public void Can_assert_class_anonymous()
+    {
+        var method =
+            typeof(AnonymousController)
+            .GetMethod(nameof(AnonymousController.Method))!;
+
+        _tested.Assert(method, _context);
+
+        Assert.Empty(_context.Messages);
+    }
+
+    [Fact]
+    public void Can_assert_method_authorize()
+    {
+        var method =
+            typeof(Controller)
+            .GetMethod(nameof(Controller.AuthorizeMethod))!;
+
+        _tested.Assert(method, _context);
+
+        Assert.Empty(_context.Messages);
+    }
+
+    [Fact]
+    public void Can_assert_method_anonymous()
+    {
+        var method =
+            typeof(Controller)
+            .GetMethod(nameof(Controller.AnonymousMethod))!;
+
+        _tested.Assert(method, _context);
+
+        Assert.Empty(_context.Messages);
+    }
+
+    [Fact]
+    public void Can_assert_method_non_action()
+    {
+        var method =
+            typeof(Controller)
+            .GetMethod(nameof(Controller.NonActionMethod))!;
+
+        _tested.Assert(method, _context);
+
+        Assert.Empty(_context.Messages);
+    }
+
+    [Fact]
+    public void Throws_on_assert_method_unspecified()
+    {
+        var method =
+            typeof(Controller)
+            .GetMethod(nameof(Controller.Method))!;
+
+        Assert.Throws<ConventionFailedException>(() => _tested.Assert(method, _context));
+        Assert.Single(_context.Messages);
+    }
+
+    [Authorize]
+    private class AuthorizeController
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Used for testing")]
+        public ActionResult Method()
+        {
+            return null!;
+        }
+    }
+
+    [AllowAnonymous]
+    private class AnonymousController
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Used for testing")]
+        public ActionResult Method()
+        {
+            return null!;
+        }
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Used for testing")]
+    private class Controller
+    {
+        [Authorize]
+        public ActionResult AuthorizeMethod()
+        {
+            return null!;
+        }
+
+        [AllowAnonymous]
+        public ActionResult AnonymousMethod()
+        {
+            return null!;
+        }
+
+        [NonAction]
+        public ActionResult NonActionMethod()
+        {
+            return null!;
+        }
+
+        public ActionResult Method()
+        {
+            return null!;
+        }
+    }
+}
---
+++ src/ConventionAssertions/Rules/HasAuthorization.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConventionAssertions.Rules;
+
+// TODO: This adds a AspNetCore reference, perhaps move to a different assembly?
+// TODO: Name of this rule? HasControllerAuthorization/HasAspNetCoreAuthorization?
+public class HasAuthorization : IMethodConvention
+{
+    public void Assert(MethodInfo method, ConventionContext context)
+    {
+        GuardAgainst.Null(method);
+        GuardAgainst.Null(context);
+
+        if (method.DeclaringType != null &&
+            HasAnyAttribute(method.DeclaringType, typeof(AuthorizeAttribute), typeof(AllowAnonymousAttribute)))
+        {
+            return;
+        }
+
+        if (HasAnyAttribute(method, typeof(NonActionAttribute)))
+        {
+            return;
+        }
+
+        if (!HasAnyAttribute(method, typeof(AuthorizeAttribute), typeof(AllowAnonymousAttribute)))
+        {
+            context.Fail(method, "No auth for controller method");
+        }
+    }
+
+    private static bool HasAnyAttribute(MemberInfo memberInfo, params Type[] attributeTypes)
+    {
+        var attributes = memberInfo.GetCustomAttributes(true)
+            .Select(x => x.GetType())
+            .ToList();
+        foreach (var attributeType in attributeTypes)
+        {
+            if (attributes.Any(x => x.IsAssignableTo(attributeType)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
