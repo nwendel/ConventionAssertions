22231e911eb299c2e12e4920ac84f01b36fa0452
niklaswendel <niklas.wendel@gmail.com>
2023-07-19 15:24:57 +0200

    MethodAssert tests

---
+++ src/ConventionAssertions.Tests/Internal/MethodAssertTests.cs
@@ -0,0 +1,34 @@
+﻿using ConventionAssertions.Internal;
+using ConventionAssertions.Tests.TestHelpers;
+using Xunit.Sdk;
+
+namespace ConventionAssertions.Tests.Internal;
+
+public class MethodAssertTests
+{
+    private readonly MethodAssert _tested = new(new DummyMethodSource());
+
+    [Fact]
+    public void Can_assert_generic_overload()
+    {
+        _tested.Assert<NothingMethodConvention>();
+    }
+
+    [Fact]
+    public void Can_assert_instance_overload()
+    {
+        _tested.Assert(new NothingMethodConvention());
+    }
+
+    [Fact]
+    public void Throws_on_assert_generic_overload()
+    {
+        Assert.Throws<XunitException>(() => _tested.Assert<FailingMethodConvention>());
+    }
+
+    [Fact]
+    public void Throws_on_assert_instance_overload()
+    {
+        Assert.Throws<XunitException>(() => _tested.Assert(new FailingMethodConvention()));
+    }
+}
---
+++ src/ConventionAssertions.Tests/TestHelpers/DummyMethodSource.cs
@@ -0,0 +1,10 @@
+﻿namespace ConventionAssertions.Tests.TestHelpers;
+
+public class DummyMethodSource : ConventionMethodSource
+{
+    public DummyMethodSource()
+        : base(new[] { typeof(DummyMethodSource) })
+    {
+        Methods = new[] { GetType().GetMethods()[0] };
+    }
+}
