37f281e45a10820c60363d51f3d8ff75ef5d641f
Niklas Wendel <niklas.wendel@gmail.com>
2023-08-04 08:07:12 +0200

    Fix PropertyInfo DisplayName (#26)

---
+++ src/ConventionAssertions/Reflection/PropertyInfoDisplayNameExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace ConventionAssertions.Reflection;
+
+public static class PropertyInfoDisplayNameExtensions
+{
+    public static string DisplayName(this PropertyInfo self)
+    {
+        GuardAgainst.Null(self);
+
+        var builder = new StringBuilder();
+
+        if (self.DeclaringType != null)
+        {
+            builder.Append(self.DeclaringType.DisplayName());
+            builder.Append('.');
+        }
+
+        // TODO: This doesn't handle indexers correctly yet
+        builder.Append(self.Name);
+
+        return builder.ToString();
+    }
+}
